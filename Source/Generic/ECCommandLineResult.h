9f1518a54fc458f6db64eb79c5e2faba77b054d9
Sam Deane <sam@elegantchaos.com>
2013-09-26 15:59:43 +0100

    Added another error code.

--- Source/Generic/ECCommandLineResult.h
+++ Source/Generic/ECCommandLineResult.h
@@ -18,4 +18,5 @@ typedef NS_ENUM(NSInteger, ECCommandLineResult)
 	ECCommandLineResultNotImplemented,
 	ECCommandLineResultMissingArguments,
 	ECCommandLineResultImplementationReturnedError,
+	ECCommandLineResultMissingBundle,
 };
