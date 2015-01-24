4c4bfe4bd4c89a57ab82378ef671680b13cdbfa9
Sam Deane <sam@elegantchaos.com>
2015-01-24 11:52:38 +0000

    Updated copyrights

--- Source/Generic/ECCommandLineMissingClassCommand.h
+++ Source/Generic/ECCommandLineMissingClassCommand.h
@@ -3,7 +3,7 @@
 //  ECCommandLine
 //
 //  Created by Sam Deane on 08/06/2013.
-//  Copyright (c) 2014 Sam Deane, Elegant Chaos. All rights reserved.
+//  Copyright (c) 2015 Sam Deane, Elegant Chaos. All rights reserved.
 //
 
 #import <ECCommandLine/ECCommandLine.h>
